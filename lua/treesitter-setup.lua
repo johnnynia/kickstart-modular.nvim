5b595c42b7040dca44e95349dadcf911a403141d
Johnny <johnny@instrumentor.ch>
2024-01-03 00:19:25 +0100

    Add more parsers.

--- lua/treesitter-setup.lua
+++ lua/treesitter-setup.lua
@@ -4,7 +4,7 @@
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'ruby', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'bash', 'c', 'cpp', 'css', 'csv', 'diff', 'dockerfile', 'gitignore', 'go', 'html', 'javascript', 'json', 'lua', 'markdown', 'mermaid', 'php', 'python', 'regex', 'ruby', 'rust', 'sql', 'svelte', 'tsx', 'typescript', 'vimdoc', 'vim', 'yaml' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
